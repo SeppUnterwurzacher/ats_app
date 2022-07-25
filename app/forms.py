9fea57e9728e15bbe392b470653d712efd4f5803
SeppUnterwurzacher <alexander.gogl@gmx.at>
2022-07-25 18:11:15 +0200

    Die Kurzprüfung erfolgt nun auf einer Seite

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
 
-class PrueferForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    submit = SubmitField('weiter')
 
-class DruckForm(FlaskForm):
+class KPEinsatzUebung(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
     druckl = IntegerField('Druck Links', validators=[DataRequired(), NumberRange(min=180)])
     druckr = IntegerField('Druck Rechts', validators=[DataRequired(), NumberRange(min=180)])
-    submit = SubmitField('weiter')
+    submit = SubmitField('abschließen')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from crypt import methods
 from flask import render_template, url_for, redirect
-from app import app
-from app.forms import DruckForm, PrueferForm
+from app import app, db
+from app.forms import KPEinsatzUebung
+from app.models import Kurzpruefung
 
 @app.route('/')
 @app.route('/index')
@@ -13,46 +13,28 @@ def auswahl():
     title = "Home"
     return render_template('auswahl.html', title=title)
 
-@app.route('/kpname', methods=['GET', 'POST'])
-def kpname():
-    title = "Einsatz 1"
-    geraet = "Gerät 1"
-    feuerwehr = "FF Trins"
-    form = PrueferForm()
+@app.route('/kpstand', methods=['GET', 'POST'])
+def kpstand():
+    form = KPEinsatzUebung()
+
     if form.validate_on_submit():
-        global data
-        data = {'name': form.name.data}
-        return redirect(url_for('kpgurte'))
-    return render_template('/kp/kpname.html', title=title, geraet=geraet, feuerwehr=feuerwehr, form=form)
+        eintrag = Kurzpruefung(
+            pruefer=form.name.data, 
+            grund='Einsatz', 
+            druck1=form.druckl.data, 
+            druck2=form.druckr.data,
+            dichtheit=True,
+            warnsignal=True,
+            id_geraet=1)
+
+        db.session.add(eintrag)
+        db.session.commit()
+
+        return redirect(url_for('test', name=name))   
+
+    return render_template('kpstandard.html', form=form)
 
 
-@app.route('/kpgurte')
-def kpgurte():
-    global data
-    name = data["name"]
-    return render_template('/kp/kpgurte.html', name=name, geraet="testgerät", feuerwehr="Testfeuerwehr", pruefer=name)
-
-
-@app.route('/kpdruck', methods=['GET', 'POST'])
-def kpdruck():
-    global data
-    name = data["name"]
-    form = DruckForm()
-    if form.validate_on_submit():
-        data["druckl"] = form.druckl.data, 
-        data["druckr"] = form.druckr.data
-        return redirect(url_for('kpdicht'))
-    return render_template('/kp/kpdruck.html', name=name, form=form, geraet="testgerät", feuerwehr="Testfeuerwehr", pruefer=name)
-
-
-@app.route('/kpdicht')
-def kpdicht():
-    global data
-    name = data["name"]
-    return render_template('/kp/kpdicht.html', geraet="testgerät", feuerwehr="Testfeuerwehr", pruefer=name)
-
-@app.route('/kpwarn')
-def kpwarn():
-    global data
-    name = data["name"]
-    return render_template('/kp/kpwarn.html', geraet="testgerät", feuerwehr="Testfeuerwehr", pruefer=name)
+@app.route('/eingetragen')
+def test():
+    return '<h1>Eintrag erfolgt</h1>'
