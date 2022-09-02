3578431810315d6fd9adf705548a6fe9130a8a4f
SeppUnterwurzacher <alexander.gogl@gmx.at>
2022-09-02 21:06:35 +0200

    Alle Gründe für Kurzprüfung nun möglich

--- app/routes.py
+++ app/routes.py
@@ -13,16 +13,21 @@ def auswahl(geraet):
     title = "Home"
     return render_template('auswahl.html', title=title, geraet=geraet)
 
-@app.route('/kpstand/<geraet>', methods=['GET', 'POST'])
-def kpstand(geraet):
+@app.route('/kpstand/<geraet>/<grund>', methods=['GET', 'POST'])
+def kpstand(geraet, grund):
     form = KPEinsatzUebung()
+
+    if grund == 'Tourlich':
+        kurz = True
+    else:
+        kurz = False
 
     if form.validate_on_submit():
         id = Geraete.query.filter(Geraete.name_geraet==geraet).all()
         id = id[0].id
         eintrag = Kurzpruefung(
             pruefer=form.name.data, 
-            grund='Einsatz', 
+            grund=grund, 
             druck1=form.druckl.data, 
             druck2=form.druckr.data,
             dichtheit=True,
@@ -34,7 +39,7 @@ def kpstand(geraet):
 
         return redirect(url_for('eingetragen'))   
 
-    return render_template('kpstandard.html', form=form, geraet=geraet)
+    return render_template('kpstandard.html', form=form, geraet=geraet, kurz=kurz)
 
 
 @app.route('/eingetragen')
