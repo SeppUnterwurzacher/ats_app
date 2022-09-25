72ddf68607e1486677d450250b8ed613d235b668
SeppUnterwurzacher <alexander.gogl@gmx.at>
2022-09-25 18:18:50 +0200

    Databas URI angepasst (statisch)

--- config.py
+++ config.py
@@ -2,7 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="agogl",
+    password="SeppSepp",
+    hostname="agogl.mysql.pythonanywhere-services.com",
+    databasename="agogl$ats",
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
