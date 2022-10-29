425d70c276cad9648ac34dd01921eae1cda2306d
SeppUnterwurzacher <alexander.gogl@gmx.at>
2022-10-29 11:45:08 +0200

    DB: Benutzer und Feuerwehr eingefügt

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Geraete(UserMixin, db.Model):
     typ_geraet = db.Column(db.String(15))
     yyyy_geraet = db.Column(db.Integer)
     password_hash = db.Column(db.String(128))
+    id_feuerwehr = db.Column(db.Integer, db.ForeignKey('feuerwehren.id'))
 
     def __repr__(self):
         return '<Geraete {}>'.format(self.name_geraet)
@@ -34,6 +35,23 @@ class Kurzpruefung(db.Model):
 
     def __repr__(self):
         return '<Kurzpruefung {}>'.format(self.zeit)
+
+class Feuerwehren(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    strasse = db.Column(db.String(30), nullable=False)
+    plz = db.Column(db.Integer, nullable=False)
+    ort = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return '<Feuerwehr {}>'.format(self.name)
+
+class Benutzer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    benutzer = db.Column(db.String(30), nullable=False)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(30), nullable=False)
+    id_feuerwehr = db.Column(db.Integer, db.ForeignKey('feuerwehren.id'))
 
 @login.user_loader
 def load_geraet(id):
