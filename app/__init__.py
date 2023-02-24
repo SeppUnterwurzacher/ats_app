859646acaff29ffceb75c0032b9679756c022a97
SeppUnterwurzacher <alexander.gogl@gmx.at>
2023-02-24 22:09:26 +0100

    Error Seiten und Log File eingebaut

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,4 +18,34 @@ fa = FontAwesome(app)
 login = LoginManager(app)
 login.login_view = 'wart_login'
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='a.gogl@feuerwehr.tirol', 
+            toaddrs=app.config['ADMINS'], 
+            subject='ATS-App Fehler',
+            credentials=auth, 
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/ats_app.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('ATS App')
